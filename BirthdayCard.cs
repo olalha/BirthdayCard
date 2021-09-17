b597e10913e3ea4dc542cf021c509b545b9522fd
olalha <68494940+olalha@users.noreply.github.com>
2021-09-17 15:10:27 +0200

    Add files via upload

---
+++ BirthdayCard.cs
@@ -0,0 +1,61 @@
+﻿using System; // What is this C# BS?! wtf
+
+namespace Happy_Bday_Jacob { // Ohh yeah - Happy Birthday! 
+    class BirthdayCard { // So yeah, this program is your birthday card... LMAO!
+        static void Main(string[] args) { // Sh*t getting heat in the Main method tho!
+            
+            bool MrD_wants_a_CRINGE_Bday_message = true; // AHAHAH He wants the cringe version!!
+            Console.WriteLine("Do you want a CRINGE AS brithday message?\nIf not, write'NONONO'");
+            String A_Chad_wrote_this = Console.ReadLine(); // Maybe he does not want it CRINGE AS...
+            if (A_Chad_wrote_this == "NONONO") { MrD_wants_a_CRINGE_Bday_message = false; }
+
+            // Lol, is this how fortnite kids say H-Bday? That is not wholesome big Chungus!
+            String Cringe_Bday_message = "Omg man, Happy Birthday to You and everything, for real lol xd!";
+
+            if (MrD_wants_a_CRINGE_Bday_message) { // Imagine actually lol
+                    Console.WriteLine(Cringe_Bday_message); // I hope it never comes to this
+            } else {
+                var actual_message = new System.Text.StringBuilder(); // Oh yeah, Bob the Builder Time!
+                String[] Lol_words_xd = Cringe_Bday_message.Split(' '); // No more cringe! Time to fix it
+
+                char some_kinda_char_idk = Lol_words_xd[1].ToCharArray()[69 / 23]; // Cheeky lil 69 there
+                String ThIs_Is_A_wOrD = Lol_words_xd[9]; // In case you didn't know, that is a word...
+
+                for(int i = 3; i < 5; i++) { // Yo a Loop?! This must be BigBrain...
+                    for(int x = 0; x < i; x++) { // It's a f**king nested loop, clearly MegaBrain!
+                        if (i < 4) { // Quick maths
+                            if (x == 2) { actual_message.Append(some_kinda_char_idk.ToString() + " "); }
+                            actual_message.Append(Lol_words_xd[5].ToCharArray()[x].ToString());
+                        } else { // Already lost? Wait, there is more...
+                            if (x % 2 == 0) { actual_message.Append(ThIs_Is_A_wOrD[x].ToString() + " "); }
+                        } // Yeah have fun trying to figure all this out in your head... Get pranked!
+                    }
+                    if (i < 420 - 416) { actual_message.Append(" "); } // Another cheeky number AHAHAH EZ
+                }
+                String Bussin_word = Char.ToUpper(Lol_words_xd[420 - 420].ToCharArray()[2]).ToString();
+                actual_message.Append(ThIs_Is_A_wOrD + " " + Bussin_word + some_kinda_char_idk + " ");
+
+                Char[] Bussin_word_round2 = Lol_words_xd[3].ToCharArray(); // I'm lovin' these Bussin words
+                actual_message.Append(Bussin_word_round2[3].ToString() + Bussin_word_round2[4].ToString());
+                actual_message.Append(Lol_words_xd[11].ToCharArray()[1337 - 1337].ToString() + " ");
+
+                int[] These_numbers_are_random_Trust_me = new int[] { 8, 7, 1, 6, 2, 3}; // Trust me man
+
+                foreach (int intIGGER in These_numbers_are_random_Trust_me) { // intIGGER, actually funny
+                    String BRUHHH = Lol_words_xd[intIGGER].Replace(some_kinda_char_idk.ToString(), "");
+                    actual_message.Append(BRUHHH + " "); // Append the BRUHH! DO IT!
+                }
+                String Yessir = Lol_words_xd[8].Replace(Lol_words_xd[10].ToCharArray()[1].ToString(), "");
+                actual_message.Insert(69 - 29, " " + Yessir); // Insert 69? Yessir!
+                actual_message.Append(Lol_words_xd[11].ToCharArray()[2].ToString()); // Final touches
+
+                System.Console.WriteLine(actual_message.ToString()); // BUM! Message de-cringed!
+            }
+        }
+    }
+}
+
+
+
+
+
